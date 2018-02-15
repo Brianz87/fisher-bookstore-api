7d7ce8674ed14f2dfd5561685742d7824f4a3595
Brian <zovko.7@osu.edu>
2018-02-14 23:52:59 -0500

    Added Api code for authors and books

---
+++ Models/Author.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Fisher.Bookstore.Api.Models
+{
+
+    public class Author
+    {
+
+
+        public int AuthorId { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public string PopularWorks { get; set; }
+    }
+}
